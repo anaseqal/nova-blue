cd17c2b4c51ef8fb9dd964f050f2846d0b17ceec
Anas Eqal <anas.eqal@gmail.com>
2019-03-01 16:04:47 +0200

    Initial commit.

---
+++ src/ThemeServiceProvider.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Anaseqal\Blue;
+
+use Laravel\Nova\Nova;
+use Laravel\Nova\Events\ServingNova;
+use Illuminate\Support\ServiceProvider;
+
+class ThemeServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        Nova::serving(function (ServingNova $event) {
+            Nova::style('blue', __DIR__.'/../resources/css/theme.css');
+        });
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
